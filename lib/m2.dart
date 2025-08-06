b6ff555c32b3f62ea1786b04f3a52d7cbe8bc79d
unknown <humb220106>
2025-08-06 15:18:32 +0100

    change made in the lib

---
+++ lib/m2.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+//  for ma26 navigation
+class PageM2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Profile')),
+      body: Center(child: Text('Read More')),
+    );
+  }
+}
+
+
+
+
+
+
+// for drawer navigation
+
+// class M2Page extends StatelessWidget {
+//   const M2Page({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("My Profile"),
+//         backgroundColor: Colors.green,
+//       ),
+//       body: const Center(child: Text("View My Profile")),
+//     );
+//   }
+// }
---
+++ lib/m6.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// / for m26 navigation
+class PageM6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Course')),
+      body: Center(child: Text('View My Course Details')),
+    );
+  }
+}
